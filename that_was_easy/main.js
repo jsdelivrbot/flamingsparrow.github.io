40468c0c8b42e09a6b57c3941d142e985743fba7
Nehal Tangudu <nehaltangudu12@gmail.com>
2017-10-31 20:43:32 +0000

    sef

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -4,3 +4,10 @@ thatwaseasy.play();
 }
 $("#easy").on("click", saythatwaseasy);
 
+$(document).keypress(delegateKeypress);
+
+function delegateKeypress(event){
+    if (event.charCode == 32){
+        $("easy").trigger("click");
+    }
+}
