36f966884dc59a2425c6092ae59c47e4b78b734a
Nehal Tangudu <nehaltangudu12@gmail.com>
2017-11-07 22:17:39 +0000

    HUUUU

--- geometric pattern/main.js
+++ geometric pattern/main.js
@@ -2,6 +2,9 @@
 var NUM_CIRCLES = 12
 var circleDiameter;
 var circleRadius;
+var rVal;
+var gVal;
+var bVal;
 
 function setup() {
   createCanvas(480, 600);
@@ -13,6 +16,9 @@ function draw() {
   var isShifted = false;
 
   var y = height;
+   rVal = 255;
+  gVal = 0;
+  bVal = 0;
   while (y >= 0) {
 
     var x;
@@ -24,11 +30,22 @@ function draw() {
     }
 
     while (x <= width) {
+      fill(color(rVal,gVal,bVal));
+stroke(color(rVal,gVal,bVal));
       ellipse(x, y, circleDiameter, circleDiameter);
       x = x + circleDiameter;
     }
 
     y = y - circleRadius;
     isShifted = !isShifted;
+      rVal = rVal - 2;
+    gVal = gVal + 7;
+    bVal = bVal + 3;
   }
+  function keyPressed() {
+  if (keyCode === 115 || keyCode === 83) {
+    saveCanvas('geometricPattern', 'png');
+  }
+  return false;
+}
 }
